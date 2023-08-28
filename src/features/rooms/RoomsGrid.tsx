ac309ff9baffe38f8acb5f8d14317e6ee3cb0372
ahmed <primaryahmedsaid@gmail.com>
2023-08-28 12:34:39 +0300

    init settings feature

--- src/features/rooms/RoomsGrid.tsx
+++ src/features/rooms/RoomsGrid.tsx
@@ -3,13 +3,13 @@ import { RoomCard } from './RoomCard'
 import { useRooms } from './hooks/useRooms'
 
 export const RoomsGrid = () => {
-	const { rooms, isFetching } = useRooms()
+	const { rooms = [], isFetching } = useRooms()
 
 	if (!rooms?.length && !isFetching) return <Empty />
 
 	return (
 		<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
-			{rooms?.map((room) => (
+			{rooms.map((room) => (
 				<RoomCard room={room} key={room.id} />
 			))}
 		</div>
