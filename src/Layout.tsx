794d16b3537f7355f68d8b114061910dc53681af
ahmed <primaryahmedsaid@gmail.com>
2023-09-06 08:28:58 +0300

    fix: fixed header padding

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -9,10 +9,13 @@ export const Layout = () => {
 			<div className='absolute z-10 top-3 left-1/2 -translate-x-1/2'>
 				<Loading />
 			</div>
+
 			<Sidebar className='sticky top-0 hidden border-r md:block h-screen animate-in slide-in-from-left' />
-			<header className='sticky top-0 flex h-full items-center space-x-5 border-b border-border px-4 md:col-start-2 md:row-start-1 md:px-8 2xl:container z-[6] bg-background'>
+
+			<header className='sticky top-0 flex h-full items-center space-x-5 border-b border-border px-4 md:col-start-2 md:row-start-1 md:px-6 2xl:container z-[6] bg-background'>
 				<Header />
 			</header>
+
 			<main className='min-h-[calc(100dvh-4.2rem)] relative p-4 md:p-6 md:col-start-2 2xl:container'>
 				<Outlet />
 			</main>
